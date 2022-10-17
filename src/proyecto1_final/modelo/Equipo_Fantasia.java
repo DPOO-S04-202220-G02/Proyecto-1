2a088991b2419e186c7985ab50206ecc9830a960
catalinavelez3 <c.velezp2@unaindes.edu.co>
2022-10-16 22:56:15 -0500

    intento :(((

--- src/proyecto1_final/modelo/Equipo_Fantasia.java
+++ src/proyecto1_final/modelo/Equipo_Fantasia.java
@@ -1,5 +1,54 @@
 package proyecto1_final.modelo;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import uniandes.dpoo.taller0.modelo.Atleta;
 
 public class Equipo_Fantasia {
+	
+	private String nombre;
+	
+	public int puntos;
+	
+	
+	private String arquero1;
+	private String arquero2;
+	private String defensor1;
+	private String defensor2;
+	private String defensor3;	
+	private String defensor4;
+	private String defensor5;
+	private String mediocampista1;
+	private String mediocampista2;
+	private String mediocampista3;
+	private String mediocampista4;
+	private String mediocampista5;
+	private String delantero1;
+	private String delantero2;
+	private String delantero3;
+
+	
+	
+
+
+
+
+
+
+	
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+		
 
 }
